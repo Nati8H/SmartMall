bbd73ebf80aded34f3ab05190962884b9e83f7ac
Nati8H <natali.hristova@petroffsoft.bg>
2023-11-16 19:32:22 +0200

    init

---
+++ src/test/java/com/ngsolutions/SmartMall/SmartMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngsolutions.SmartMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
